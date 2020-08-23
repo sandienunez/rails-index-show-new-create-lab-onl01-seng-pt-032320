cef2d951632ef50bcedaf7269b340f5fb905f1bf
github username <github email address>
2020-08-22 22:00:17 -0400

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,11 @@
+class CouponsController < ActionController::Base
+
+    def create
+        # @student = Student.new
+        # @student.first_name = params[:first_name]
+        # #binding.pry
+        # @student.last_name = params[:last_name]
+        # @student.save
+        # redirect_to student_path(@student)
+      end
+end
